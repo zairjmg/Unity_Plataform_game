60d3174af0413429ed2168437c2d8791c696b90e
zairjmg <jair.jmgu@gmail.com>
2021-12-18 23:08:54 -0500

    _

--- Assets/Scripts/player_move.cs
+++ Assets/Scripts/player_move.cs
@@ -15,6 +15,7 @@ public class player_move : MonoBehaviour
     public float Speed;
     private float h;
     private bool _grounded;
+    private bool _facingRight = true;
 
     /* parametro */
     private Rigidbody2D _rigidbody;
@@ -39,6 +40,16 @@ public class player_move : MonoBehaviour
         h = Input.GetAxisRaw("Horizontal");
         Debug.Log("h: " + h);
 
+        /* Girar sprite */
+        if (h < 0.0f && _facingRight == true)
+        {
+            flip();
+        } 
+        else if (h > 0.0f && _facingRight == false)
+        {
+            flip();
+        }
+
         /* Detectar suelo */
         _grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
@@ -49,13 +60,24 @@ public class player_move : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector2(h * Speed, _rigidbody.velocity.y);
+    }
+
+    private void LateUpdate() {
+        _animator.SetBool("Running", h != 0.0f);
+        _animator.SetInteger("Idle", ((int)h));
+    }
+
     private void Jump() {
         _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
-    private void FixedUpdate()
-    {
-        _rigidbody.velocity = new Vector2(h * Speed, _rigidbody.velocity.y);
-        _animator.SetBool("Running", h != 0.0f);
+    private void flip() {
+        _facingRight = !_facingRight;
+        float _localScaleX = transform.localScale.x;
+        _localScaleX = _localScaleX * -1f;
+        transform.localScale = new Vector3(_localScaleX, transform.localScale.y, transform.localScale.z);
     }
 }
